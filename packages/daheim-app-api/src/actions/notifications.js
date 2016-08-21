e43cb1aa3d220f9bcd09a9f22c373286e6ac08d9
Gergo Ertli <egergo@gmail.com>
2016-08-21 13:45:04 +0300

    Handling webpush error responses

--- packages/daheim-app-api/src/actions/notifications.js
+++ packages/daheim-app-api/src/actions/notifications.js
@@ -1,18 +1,24 @@
 import webPush from 'web-push'
+import log from '../log'
 
 webPush.setGCMAPIKey(process.env.GCM_API_KEY)
 
 export default (def) => {
   def('/notifications.test', async (req, res) => {
     const {endpoint, userPublicKey, userAuth} = req.body
-    await webPush.sendNotification(endpoint, {
-      TTL: 60 * 2,
-      payload: JSON.stringify({
-        type: 'test'
-      }),
-      userPublicKey,
-      userAuth
-    })
+    try {
+      await webPush.sendNotification(endpoint, {
+        TTL: 60 * 2,
+        payload: JSON.stringify({
+          type: 'test'
+        }),
+        userPublicKey,
+        userAuth
+      })
+    } catch (err) {
+      const {statusCode, headers, body} = err
+      log.error({err, statusCode, headers, body}, 'cannot send webpush notification')
+    }
     return {}
   })
 }
