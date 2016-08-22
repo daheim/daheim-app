17599879e6c6a6bc8f48324bbce1e9e6ce65ba62
Gergo Ertli <egergo@gmail.com>
2016-08-22 21:28:59 +0300

    Improved modal dialog
    DHM-21

--- packages/daheim-app-ui/src/Modal.js
+++ packages/daheim-app-ui/src/Modal.js
@@ -1,9 +1,36 @@
 import React, {PropTypes, Component} from 'react' // eslint-disable-line no-unused-vars
+import CloseIcon from 'material-ui/svg-icons/navigation/close'
 import Modal from 'react-modal'
 import modalCss from './Modal.style'
 
 export default class DhmModal extends Component {
+
+  static propTypes = {
+    closeIcon: PropTypes.bool,
+    onRequestClose: PropTypes.func,
+    children: PropTypes.node
+  }
+
+  static defaultProps = {
+    closeIcon: true
+  }
+
+  handleCloseIconClick = (e) => {
+    if (this.props.onRequestClose) this.props.onRequestClose('closeIcon')
+  }
+
   render () {
-    return <Modal overlayClassName={modalCss.overlay} className={modalCss.container} {...this.props} />
+    const {children, closeIcon, ...otherProps} = this.props
+
+    return (
+      <Modal overlayClassName={modalCss.overlay} className={modalCss.container} {...otherProps}>
+        <div className={modalCss.inner}>
+          {closeIcon
+            ? <div className={modalCss.closeIconContainer}><CloseIcon className={modalCss.closeIcon} color='gray' onClick={this.handleCloseIconClick} /></div>
+            : null}
+          {children}
+        </div>
+      </Modal>
+    )
   }
 }
