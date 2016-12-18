b4d601f389aa112c70b107ebe03c7d03217e04db
k127 <k127@gmx.de>
2016-12-19 00:29:26 +0100

    #6 convert strings to translatables in TalkAbout

--- packages/daheim-app-ui/src/components/TalkAbout.js
+++ packages/daheim-app-ui/src/components/TalkAbout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {FormattedMessage} from 'react-intl'
 
 import Weather from './Weather'
 
@@ -6,35 +7,35 @@ export default class TalkAbout extends React.Component {
   render () {
     return (
       <div {...this.props}>
-        <h2>Über was kann man reden?</h2>
+        <h2><FormattedMessage id='talkAbout.whatToTalkAbout' /></h2>
         <div style={{display: 'flex', flexWrap: 'wrap'}}>
           <div style={{flex: '0 0 200px', margin: 8}}>
-            <h3>...das Alphabet</h3>
+            <h3><FormattedMessage id='talkAbout.alphabet' /></h3>
             <div style={{fontSize: 30, fontFamily: 'monospace'}}>A B C D E<br />F G H I J<br />K L M N O<br />P Q R S T<br />U V W X Y<br />Z Ä Ö Ü ß</div>
           </div>
           <div style={{flex: '1 1 500px', margin: 8}}>
-            <h3>...das Wetter</h3>
+            <h3><FormattedMessage id='talkAbout.weather' /></h3>
             <Weather />
           </div>
           <div style={{flex: '1 1 200px', margin: 8}}>
-            <h3>...Deutschland</h3>
+            <h3><FormattedMessage id='talkAbout.germany' /></h3>
             <div>
-              <li>die Bundesländer</li>
-              <li>Hauptstadt Berlin</li>
+              <li><FormattedMessage id='talkAbout.federalStates' /></li>
+              <li><FormattedMessage id='talkAbout.capitalCityBerlin' /></li>
             </div>
           </div>
           <div style={{flex: '1 1 200px', margin: 8}}>
-            <h3>...wichtige Dinge</h3>
+            <h3><FormattedMessage id='talkAbout.importantStuff' /></h3>
             <div>
-              <li><a href='http://www.fluechtlinge-willkommen.de/' target='_blank'>Flüchtlinge Willkommen</a></li>
+              <li><a href='http://www.fluechtlinge-willkommen.de/' target='_blank'><FormattedMessage id='talkAbout.refugeesWelcome' /></a></li>
             </div>
           </div>
           <div style={{flex: '1 1 200px', margin: 8}}>
-            <h3>...Essen und Trinken</h3>
+            <h3><FormattedMessage id='talkAbout.eatAndDrink' /></h3>
             <div>
-              <li>Brot</li>
-              <li>Wurst</li>
-              <li>Apfel</li>
+              <li><FormattedMessage id='talkAbout.bread' /></li>
+              <li><FormattedMessage id='talkAbout.sausage' /></li>
+              <li><FormattedMessage id='talkAbout.apple' /></li>
             </div>
           </div>
         </div>
