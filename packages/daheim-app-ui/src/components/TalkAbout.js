14eb871625446db8ff22ff783c4b1644387c7f90
Eugen Kiss <eugen@eugenkiss.com>
2017-02-19 21:23:45 +0100

    Make 'talk about' texts translatable

--- packages/daheim-app-ui/src/components/TalkAbout.js
+++ packages/daheim-app-ui/src/components/TalkAbout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FormattedMessage} from 'react-intl'
+import {FormattedMessage, FormattedHTMLMessage} from 'react-intl'
 import styled from 'styled-components'
 
 import {H2, H3, Flex, VSpace, Text} from './Basic'
@@ -25,18 +25,18 @@ const Container = styled.div`
 `
 
 export default class TalkAbout extends React.Component {
-  renderEntry(image, title, body) {
+  renderEntry(image, titleId, bodyId) {
     return (
       <Flex
         column align='center' justify='start'
         style={{width: Layout.widthPx / 3.9, marginBottom: rowSpacing, marginRight: colSpacing}}
         >
-        <H3>{title}</H3>
+        <H3><FormattedMessage id={titleId}/></H3>
         <VSpace v={Padding.s}/>
         <Image src={`/talk/Gesprächsseite_Icons-${image}.svg`}/>
         <VSpace v={Padding.s}/>
         <div style={{width: '100%'}}>
-          {body}
+          <Text><FormattedHTMLMessage id={bodyId}/></Text>
         </div>
       </Flex>
     )
@@ -48,46 +48,12 @@ export default class TalkAbout extends React.Component {
         <H2><FormattedMessage id='ready.talk.about'/></H2>
         <VSpace v={Padding.m}/>
         <Container>
-          {this.renderEntry('01', 'Dein Tag',<Text>
-            Wie geht es Dir?<br/>
-            Was machst Du heute?<br/>
-            Was machst Du heute Abend?
-          </Text>)}
-
-          {this.renderEntry('02', 'Deutschland',<Text>
-            Wie gefällt Dir Deutschland?<br/>
-            Was ist neu für Dich?<br/>
-            Was ist anders?<br/>
-            Aktuelle Themen
-          </Text>)}
-          {this.renderEntry('05', 'Alltag',<Text>
-            Im Bus<br/>
-            Beim Bäcker<br/>
-            Im Supermarkt<br/>
-            Beim Arzt
-          </Text>)}
-          {this.renderEntry('06', 'Sprich doch über',<Text>
-            Was isst Du gerne?<br/>
-            Kochst Du gerne?<br/>
-            Welches Essen ist neu für Dich?<br/>
-            Welches Essen fehlt Dir?<br/>
-            Was isst Du heute Abend?
-          </Text>)}
-          {this.renderEntry('04', 'Fest und Feiertage',<Text>
-            Geburtstag<br/>
-            Namenstag<br/>
-            Weihnachten<br/>
-            Ramadan<br/>
-            Ostern<br/>
-            Chanukka<br/>
-            Andere Feiertage?
-          </Text>)}
-          {this.renderEntry('03', 'Essen und Trinken',<Text>
-            Deine Schule<br/>
-            Deine Freunde<br/>
-            Deinen Beruf<br/>
-            Deine Kultur
-          </Text>)}
+          {this.renderEntry('01', 'ready.talk.yourday.title', 'ready.talk.yourday.body')}
+          {this.renderEntry('02', 'ready.talk.germany.title', 'ready.talk.germany.body')}
+          {this.renderEntry('05', 'ready.talk.everyday.title', 'ready.talk.everyday.body')}
+          {this.renderEntry('06', 'ready.talk.food.title', 'ready.talk.food.body')}
+          {this.renderEntry('04', 'ready.talk.holidays.title', 'ready.talk.holidays.body')}
+          {this.renderEntry('03', 'ready.talk.whynot.title', 'ready.talk.whynot.body')}
         </Container>
       </div>
     )
